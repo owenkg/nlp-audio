db41747b98a28f440dd5c8f67b326aa239520789
George Owen Kivumbi <gokivumbi@Georges-MacBook-Pro.local>
2021-09-21 12:34:57 +0300

    ll

--- src/Components/box.js
+++ src/Components/box.js
@@ -7,7 +7,7 @@ const AudioCard = (props) => {
         <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
 
             <div className="icon-box">
-                <h2><i>{  props.id}</i></h2>
+                <h2><i></i></h2>
                 <div className="icon row"><i className="bi-soundwave"><h4 className="title"><a href="">{  props.name}</a></h4></i>
                 </div>
 
@@ -19,7 +19,7 @@ const AudioCard = (props) => {
                     <ListGroupItem data-aos="fade-up">
 
                         <i className="bi bi-translate"></i> <a href="#" data-bs-toggle="collapse" className="collapse show" data-bs-target={`#one${  props.id}`}>Translate<i className="bx bx-chevron-down icon-show"></i></a>
-                        <div id={`one${  props.id}`} className="collapse">
+                        <div className="collapse">
                             <textarea className="form-control" name="message" rows="5" placeholder="Translate" required></textarea>
                         </div>
 
@@ -28,7 +28,7 @@ const AudioCard = (props) => {
                     <ListGroupItem data-aos="fade-up">
 
                         <i className="bi bi-geo-alt"></i> <a href="#" data-bs-toggle="collapse" className="collapse show" data-bs-target={`#two${  props.id}`}>Location<i className="bx bx-chevron-down icon-show"></i></a>
-                        <div id={`two${  props.id}`} className="collapse">
+                        <div className="collapse">
                             <input type="text" className="form-control" name="subject" id="subject" placeholder="Location" />
                         </div>
 
@@ -36,7 +36,7 @@ const AudioCard = (props) => {
 
                     <ListGroupItem data-aos="fade-up">
                         <i className="bi-mic"></i> <a href="#" data-bs-toggle="collapse" className="collapse show" data-bs-target={`#three${  props.id}`}>Who's Speaking?<i className="bx bx-chevron-down icon-show"></i></a>
-                        <div id={`three${  props.id}`} className="collapse">
+                        <div className="collapse">
                             <div className="radio">
                                 <label><input type="radio" name="r" /> Public</label>
                             </div>
